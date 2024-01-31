73d0c962ea76878ee87f4365713eaf8fe0211cf5
Tolga <tolgaaksu.1995@gmail.com>
2024-01-31 19:43:10 +0300

    Add project files.

---
+++ NLayer.Repository/Configurations/ProductFeatureConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NLayer.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Repository.Configurations
+{
+    internal class ProductFeatureConfiguration : IEntityTypeConfiguration<ProductFeature>
+    {
+        public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<ProductFeature> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).UseIdentityColumn();
+            builder.HasOne(x => x.Product).WithOne(x => x.ProductFeature).HasForeignKey<ProductFeature>(x => x.ProductId);
+        }
+    }
+}
---
+++ NLayer.Service/Services/ProductServiceWithNoCaching.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using NLayer.Core.DTOs;
+using NLayer.Core.Models;
+using NLayer.Core.Repositories;
+using NLayer.Core.Services;
+using NLayer.Core.UnitOfWorks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Service.Services
+{
+    public class ProductServiceWithNoCaching : Service<Product>, IProductService
+    {
+        private readonly IProductRepository _productRepository;
+        private readonly IMapper _mapper;
+        public ProductServiceWithNoCaching(IGenericRepository<Product> repository, IUnitOfWork unitOfWork, IMapper mapper, IProductRepository productRepository) : base(repository, unitOfWork)
+        {
+            _mapper = mapper;
+            _productRepository = productRepository;
+        }
+
+        public async Task<CustomResponseDto<List<ProductWithCategoryDto>>> GetProductWithCategory()
+        {
+            var product = await _productRepository.GetProductsWithCategory();
+            var productDto = _mapper.Map<List<ProductWithCategoryDto>>(product);
+            return CustomResponseDto<List<ProductWithCategoryDto>>.Success(200,productDto);
+        }
+    }
+}
